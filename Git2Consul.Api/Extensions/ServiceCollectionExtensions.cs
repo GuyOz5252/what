87ddb223dfd8bc89caa11b5fcfc7085d615d19e9
Guy Oz <109523947+GuyOz5252@users.noreply.github.com>
2025-06-08 22:41:26 +0300

    Added Git2Consul

--- Git2Consul.Api/Extensions/ServiceCollectionExtensions.cs
+++ Git2Consul.Api/Extensions/ServiceCollectionExtensions.cs
@@ -21,7 +21,7 @@ public static class ServiceCollectionExtensions
             {
                 serviceCollection.AddConsul(git2ConsulEnvironment.Name, config =>
                 {
-                    config.Address = new Uri(git2ConsulEnvironment.GitRepositoryUrl);
+                    config.Address = new Uri(git2ConsulEnvironment.ConsulAddress);
                     config.Token = git2ConsulEnvironment.ConsulAclToken;
                 });
                 serviceCollection.AddKeyedTransient<ConsulKeyValueRepository>(
